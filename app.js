976ea858fc69c2be50e2a9bcd9c6357f32da6210
김민지 <qniomo00@gmail.com>
2025-02-18 16:54:34 +0900

    server 생성, port 지정

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 const http = require("http");
 const fs = require("fs");
+
+const server = http.createServer((req, res) => {});
+
+PORT = 3020;
+server.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
