ab4a8bed05d9a00321a84fb10034bda0fa7a97dd
김민지 <qniomo00@gmail.com>
2025-02-19 11:41:18 +0900

    /에 대한 응답 작성

--- test.js
+++ test.js
@@ -1,8 +1,20 @@
 const http = require("http");
 const fs = require("fs");
 
+//[ ] 페이지를 js에서 만들면
+//+로 링크 이동 -> button누르면 데이터 가져옴
+//가져온 데이터로 a태그의 href바뀜 -> innerText 변경
+
 const server = http.createServer((req, res) => {
   console.log(`${req.method}  ${req.url}`);
+  if (req.method === "GET") {
+    if (req.url === "/") {
+      let page = fs.readFileSync("test.html");
+      res.writeHead(200, { "content-type": "utf-8; text/html" });
+      res.write(page);
+      res.end();
+    }
+  }
 });
 
 PORT = 3030;
