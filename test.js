fa937656bcefbd5b5120b2d4c4ea46fe728fbd7d
김민지 <qniomo00@gmail.com>
2025-02-19 11:30:04 +0900

    req에 대한 console

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  console.log(`${req.method}  ${req.url}`);
+});
 
 PORT = 3030;
 server.listen(200, () => {
